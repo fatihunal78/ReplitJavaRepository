bfc26267243b56c61070cfcbcfe841998c0d3672
fatihunal <fatihunal@gmail.com>
2021-09-08 11:28:57 +0100

    Replit Java Repository Created These are the codes that I have written for Replit exercies. The class names show the name, title and order number of the examples.

--- src/_0_001_Basics_HelloWorld1.java
+++ src/_0_001_Basics_HelloWorld1.java
@@ -1,6 +1,6 @@
 //Write a statement that prints Hello world to the screen.
 //Output must exactly match the requirement
-public class HelloWorld1 {
+public class _0_001_Basics_HelloWorld1 {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
